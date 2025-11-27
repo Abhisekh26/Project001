cfcd95f830d6b67c316472f89450fced8269d743
Abhisekh Singh <abhisekhjimmy@gmail.com>
2025-11-27 19:34:41 +0530

    dynamics routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()                   //creates an instances of expree js application 
 
 
-app.get("/user",(req,res)=>{
-    console.log(req.query)
+app.get("/user/:userid",(req,res)=>{
+    console.log(req.params)
     res.send({firstName:"abhisekh",lastName:"singh"})
 })
 app.post("/user",(req,res)=>{

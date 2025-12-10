ff9bfe2aa734be9f8d523ddbf3d43010cb33a96e
Abhisekh Singh <abhisekhjimmy@gmail.com>
2025-12-10 10:38:14 +0530

    api to delte and post

--- src/app.js
+++ src/app.js
@@ -1,37 +1,82 @@
 const express = require('express')
-const connectdb= require("./config/database")
+const connectdb = require("./config/database")
 const users = require("./models/user")
-const app = express()                   //creates an instances of expree js application 
+const app = express()
+app.use(express.json())
+//creates an instances of expree js application 
 // const {Authuser}= require('./middlewares/userauth')
 // app.use("/user",Authuser)
 // app.get("/user/:userid",(req,res)=>{
 //     console.log(req.params)
 //     res.send("user 01 is logged")
 // })
-app.post("/signup",async(req,res)=>{
-    const userobj ={
-        firstName:"Rohit",
-        lastName:"Sharma",
-        emailId:"rohit@sharma.com",
-        password:'rohit@123'
+app.post("/signup", async (req, res) => {
+    // console.log(req.body)
+    // const userobj ={
+    //     firstName:"Rohit",
+    //     lastName:"Sharma",
+    //     emailId:"rohit@sharma.com",
+    //     password:'rohit@123'
+    // }
+    // creating anew instance of user model 
+    try {
+        const user = new users(req.body)
+        await user.save()
+        res.send("user added ")
     }
-// creating anew instance of user model 
-try{
-   const user = new users(userobj)
-   await user.save()
-    res.send("user added ")
-}
-catch(err){
-    res.status(400).send("something went wrong")
-} 
+    catch (err) {
+        res.status(400).send("something went wrong")
+    }
 })
 
-connectdb().then(()=>{
-  console.log("connection established")
-     app.listen(3000,()=>{
-    console.log("server has started")
+app.get("/user", async (req, res) => {
+    const email = req.body.emailId
+     console.log(email)
+    try {
+       
+       const user = await users.findOneAndDelete({emailId:email})
+       
+
+
+        if(user){
+             res.send("user deleted")
+        }
+        else {
+            res.send("no user")
+        }
+        
+    }
+    catch (err) {
+        res.status(400).send("something went wrong")
+    }
 })
-}).catch((err)=>{
+
+app.get("/feed", async (req, res) => {
+
+    try {
+        const user = await users.find({})
+        res.send(user)
+    }
+    catch (err) {
+        res.status(400).send("something went wrong")
+
+    }
+
+})
+
+
+
+
+
+
+
+
+connectdb().then(() => {
+    console.log("connection established")
+    app.listen(3000, () => {
+        console.log("server has started")
+    })
+}).catch((err) => {
     console.log("error happened")
 })
 

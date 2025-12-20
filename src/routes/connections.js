df0b78bce94291bde15073e4cda35ef3c491d6e1
Abhisekh Singh <abhisekhjimmy@gmail.com>
2025-12-20 19:03:52 +0530

    api

--- src/routes/connections.js
+++ src/routes/connections.js
@@ -1,26 +1,59 @@
 const connectionrequest = require("../models/connectionrequest")
-const {Authuser} = require("../middlewares/userauth")
+const Users = require("../models/user")
+const { Authuser } = require("../middlewares/userauth")
 const express = require("express")
 const connectionauth = express.Router()
 
-connectionauth.post("/request/send/:status/:touser_id",Authuser,async(req,res)=>{
-    try{
-        const fromUser= req.user._id
-        const toUser =req.params.touser_id
+connectionauth.post("/request/send/:status/:touser_id", Authuser, async (req, res) => {
+    try {
+        const fromUser = req.user._id
+        const toUser = req.params.touser_id
         const status = req.params.status
 
-    const connection= new connectionrequest({
-        fromuserid:fromUser,
-        toUserid:toUser,
-        status:status
-    })
+        if(fromUser == toUser){
+            return res.send("invalid request")
+        }
 
-    await connection.save()
-    res.send("Done")
-    }catch(err){
-        res.status(400).send("we will see")
+        const allowedStatus = ["interested", "ignore", "rejected", "accepted"]
+        const checkStatus = allowedStatus.includes(status)
+        if (!checkStatus) {
+            return res.send("enter a vlaid request")
+        }
+
+
+        const toUserexist = await Users.findById(toUser)
+        if (!toUserexist) {
+            return res.send("oops")
+        }
+
+        const existIndb = await connectionrequest.findOne({
+            $or:[{ fromuserid: fromUser, toUserid: toUser } ,{
+                 fromuserid:toUser,toUserid:fromUser
+            }]
+        })
+        if (existIndb) {
+           return  res.send("this exists")
+        }
+
+
+
+
+
+
+
+
+        const connection = new connectionrequest({
+            fromuserid: fromUser,
+            toUserid: toUser,
+            status: status
+        })
+
+        await connection.save()
+        res.send("Done")
+    } catch (err) {
+        res.status(400).send("something went wrong ")
     }
 
 })
 
-module.exports=connectionauth
+module.exports = connectionauth

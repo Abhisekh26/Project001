17bafe6a3cb5ebcc7a02f38f909bee09f85a35e2
Abhisekh Singh <abhisekhjimmy@gmail.com>
2025-12-23 12:38:10 +0530

    feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const userRouter = express.Router()
 const connectionrequest = require("../models/connectionrequest")
+const users= require("../models/user")
 const {Authuser} = require("../middlewares/userauth")
 
 //get pending friend request of logged in user 
@@ -53,6 +54,32 @@ console.log(friends)
     catch(Err){
         res.status(400).send("something went wrong")
     }
+})
+
+
+userRouter.get("/feed",Authuser,async(req,res)=>{
+  const user = req.user 
+  const page = parseInt(req.query.page) || 1
+  const limit = parseInt(req.query.limit) || 10 
+  limit = limit > 50 ?50 :limit;
+  const skip = (page-1) * limit;
+  const data = await connectionrequest.find({$or:[
+    {fromuserid:user._id},{toUserid:user._id}
+  ]}).select("fromuserid toUserid")
+ 
+
+  const hideFromUser = new Set()
+  data.forEach((key)=>{
+   
+ hideFromUser.add(key.fromuserid.toString())
+  hideFromUser.add(key.toUserid.toString())
+
+  })
+  // console.log(hideFromUser)
+ const hideData= Array.from(hideFromUser)
+const alluser = await users.find({_id:{$nin:hideData}}).select("firstName LastName").skip(skip).limit(limit)
+console.log(alluser)
+res.send(data)
 })
 
 module.exports= userRouter
